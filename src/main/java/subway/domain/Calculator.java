f3bcf563ed7f17da8705a331063ca8cd2e24c83c
raccon <minsungt@naver.com>
2023-12-08 17:40:34 +0900

    code modify

--- src/main/java/subway/domain/Calculator.java
+++ src/main/java/subway/domain/Calculator.java
@@ -1,28 +1,19 @@
 package subway.domain;
 
 import java.util.List;
+import java.util.function.ToIntFunction;
 import java.util.stream.IntStream;
 
 public class Calculator {
     public int calculateTime(List<String> shortestPath) {
-        return IntStream.range(0, shortestPath.size() - 1)
-                .map(index -> {
-                    String currentStation = shortestPath.get(index);
-                    String nextStation = shortestPath.get(index + 1);
-
-                    return LineRepository.lines().stream()
-                            .flatMap(line -> line.stations().stream())
-                            .filter(station ->
-                                    station.upStation().getName().equals(currentStation) &&
-                                            station.downStation().getName().equals(nextStation))
-                            .mapToInt(SubwayLine::getTime)
-                            .findFirst()
-                            .orElse(0);
-                })
-                .sum();
+        return calculate(shortestPath, SubwayLine::getTime);
     }
 
     public int calculateDistant(List<String> shortestPath) {
+        return calculate(shortestPath, SubwayLine::getDistant);
+    }
+
+    private int calculate(List<String> shortestPath, ToIntFunction<SubwayLine> function) {
         return IntStream.range(0, shortestPath.size() - 1)
                 .map(index -> {
                     String currentStation = shortestPath.get(index);
@@ -33,7 +24,7 @@ public class Calculator {
                             .filter(station ->
                                     station.upStation().getName().equals(currentStation) &&
                                             station.downStation().getName().equals(nextStation))
-                            .mapToInt(SubwayLine::getDistant)
+                            .mapToInt(function)
                             .findFirst()
                             .orElse(0);
                 })
